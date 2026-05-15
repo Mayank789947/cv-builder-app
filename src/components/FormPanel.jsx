5d595b7f24f8960d816a63ddbecb851040a31cf5
Mayank Rawat <rawatmayankofc25@gmail.com>
2026-05-15 19:40:30 +0530

    Modify Form Panel component
    modify form panel component with more suitable data structure pattern and output component layout

--- src/components/FormPanel.jsx
+++ src/components/FormPanel.jsx
@@ -3,6 +3,7 @@ import PersonDetails from "./PersonDetails";
 import CVPreview from "./CVPreview";
 import Education from "./Education";
 import Work from "./Work";
+import Skill from "./Skill";
 
 function FormPanel() {
     const [person, setPerson] = useState({
@@ -31,16 +32,25 @@ function FormPanel() {
             endDate: ""
         }
     ]);
-    
+
+    const [skills, setSkills] = useState([
+        {
+            skill: ""
+        }
+    ]);
+
     return (
         <>
-            <div className="form-panel">
-                <PersonDetails person={person} setPerson={setPerson} />
-                <Education institute={institute} setInstitute={setInstitute} />
-                <Work work={work} setWork={setWork} />
-            </div>
-            <div className="cv-preview">
-                <CVPreview person={person} institute={institute} work={work} />
+            <div className="main-content">
+                <div className="form-panel">
+                    <PersonDetails person={person} setPerson={setPerson} />
+                    <Education institute={institute} setInstitute={setInstitute} />
+                    <Work work={work} setWork={setWork} />
+                    <Skill skills={skills} setSkills={setSkills} />
+                </div>
+                <div className="cv-preview">
+                    <CVPreview person={person} institute={institute} work={work} skills={skills} />
+                </div>
             </div>
         </>
     )
