dc10701f7be57b6ec2db231d7ef799e25985463c
Mayank Rawat <rawatmayankofc25@gmail.com>
2026-05-16 18:38:46 +0530

    Add Skills Component
    add skills component to allow user to add their soft skills in their CV

---
+++ src/components/Skills.jsx
@@ -0,0 +1,74 @@
+import { useState } from "react";
+
+function Skills({ skills, setSkills }) {
+
+    const [isOpen, setIsOpen] = useState(false);
+
+    function toggleSection() {
+        setIsOpen(!isOpen);
+    }
+
+    function handleChange(index, e) {
+        const updatedSetSkills = [...skills];
+
+        updatedSetSkills[index][e.target.name] = e.target.value;
+
+        setSkills(updatedSetSkills);
+    }
+
+    function handleClick() {
+        setSkills([
+            ...skills,
+            {
+                skill: ""
+            }
+        ]);
+    }
+
+    function handleDelete(indexToDelete) {
+        const filteredSkills = skills.filter(
+            (_, index) => index !== indexToDelete
+        );
+
+        setSkills(filteredSkills);
+    }
+
+    return (
+        <>
+            <div className="section-header" onClick={toggleSection}>
+                <p>Soft Skills</p>
+                <p>{isOpen ? " ▲" : " ▼"}</p>
+            </div>
+
+                {isOpen && skills.map((item, index) => (
+                    <div className="section-content" key={index}>
+
+                        <input
+                            type="text"
+                            name="skill"
+                            value={item.skill}
+                            onChange={(e) => handleChange(index, e)}
+                            placeholder="Eg: team work"
+                        />
+
+                        {skills.length > 1 && (
+                            <button className="dlt-skill" onClick={() => handleDelete(index)}>
+                                Delete
+                            </button>
+                        )}
+
+                        {index === skills.length - 1 && (
+                            <button
+                                className="add-skill"
+                                onClick={handleClick}
+                            >
+                                + Add More Skills
+                            </button>
+                        )}
+                    </div>
+                ))}
+        </>
+    )
+}
+
+export default Skills;
