48b161a4da89033409a8e8b5405054e5bb1f3d68
Mayank Rawat <rawatmayankofc25@gmail.com>
2026-05-18 10:05:30 +0530

    Update import/export of App component
    update import/export statements and clean up unnecessary code

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import './App.css'
+import CVPreview from './components/CVPreview'
+import FormPanel from './components/FormPanel'
 import Header from './components/Header'
 
 function App() {
   return (
+    <>
     <Header />
+    <FormPanel />
+    </>
   )
 }
 
